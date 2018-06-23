ed3a0476f3700cb1b27b82167855be8f2f927c8b
Arif Rohman M <arif.rohman02@gmail.com>
2018-06-23 16:05:01 +0700

    fix transaksi pinjaman admin

--- modul/pinjaman/pinjaman.php
+++ modul/pinjaman/pinjaman.php
@@ -48,88 +48,107 @@ button span.ui-icon {
 <?php
 
 ini_set('display_errors', 1); ini_set('error_reporting', E_ERROR);
-echo "<div id='dalam_content'>
-<h2>DAFTAR PINJAMAN ANGGOTA</h2>
-<div id='tabs'>
-<ul>
-<li><a href='#tabs-1'>Pinjaman</a></li>
-<li><a href='#tabs-2'>Ketentuan</a></li>
-</ul>
-<div id='tabs-1'>
-<div id='form_isian'>
-<div id='info_anggota'></div>
-<table width='100%'>
-<tr>
-<td width='15%'>No.Pinjaman</td>
-<td width='2%'>:</td>
-<td><input type='text' id='no' size='15' disabled></td>
-</tr>
-<tr>
-<td width='15%'>Nomor Anggota</td>
-<td width='2%'>:</td>
-<td><input type='text' id='nomor' size='15' class='input'></td>
-</tr>
-<tr>
-<td width='15%'>Tanggal</td>
-<td width='2%'>:</td>
-<td><input type='tgl' id='tgl' size='12' class='input'></td>
-</tr>
-<tr>
-<td width='15%'>Lama Pinjaman</td>
-<td width='2%'>:</td>
-<td><select name='lama' id='lama' class='input'>
-<option value=''>-Pilih-</option>
-<option value='6'>6 Bulan</option>
-<option value='12'>12 Bulan</option>
-<option value='24'>24 Bulan</option>
-</select>
-</td>
-</tr>
-<tr>
-<td width='15%'>Bunga</td>
-<td width='2%'>:</td>
-<td><input type='text' name='bunga' id='bunga' size='5' class='input'> %</td>
-</tr>
-<tr>
-<td width='15%'>Jumlah</td>
-<td width='2%'>:</td>
-<td><input type='text' name='jml' id='jml' size='15' class='input'></td>
-</tr>
-<tr>
-<td colspan='3' align='center'>
-<button class='ui-state-default ui-corner-all' id='simpan'>
-<span class='ui-icon ui-icon-disk'></span>Simpan
-</button>
-<button class='ui-state-default ui-corner-all' id='tutup'>
-<span class='ui-icon ui-icon-circle-close'></span>Tutup
-</button>
-</td>
-</tr>
-</table>
-</div>
-<div id='menu-tombol1'>
-<div id='tombol-tambah'>
-<button class='ui-state-default ui-corner-all' id='tambah'>
-<span class='ui-icon ui-icon-circle-plus'></span>Tambah
-</button>
-</div>
-<div id='tombol-cari'>
-Tanggal <input type='text' id='tgl1' size='10'> s/d <input type='text' id='tgl2' size='10'>
-<button class='ui-state-default ui-corner-all' id='cari2'>
-<span class='ui-icon ui-icon-search'></span>Cari
-</button>
-</div>
-</div>
-<div id='tampil_data1'></div>
-</div>
-<div id='tabs-2'>
-<p>Ketentuan :</p>
-<p>Setiap Anggota meminjam harus memenuhi persyaratan sebagai berikut</p>
-<ol>
-<li>Foto Copy KTP Suami/Istri</li>
-<li>Tidak Memiliki Tunggakan Sebelumnya</li>
-</ol>
-</div>
-</div>
+echo "
+<div id='dalam_content'>
+	<h2>DAFTAR PINJAMAN ANGGOTA</h2>
+	<div id='tabs'>
+		<ul>
+		<li><a href='#tabs-1'>Pinjaman</a></li>
+		<li><a href='#tabs-2'>Ketentuan</a></li>
+		</ul>
+		<div id='tabs-1'>
+			<div id='form_isian'>
+				<div id='info_anggota'></div>
+				<table width='100%'>
+					<tr>
+						<td width='15%'>No.Pinjaman</td>
+						<td width='2%'>:</td>
+						<td><input type='text' id='no' size='15' disabled></td>
+					</tr>
+					<tr>
+						<td width='15%'>Nomor Anggota</td>
+						<td width='2%'>:</td>
+						<td><input type='text' id='nomor' size='15' class='input'></td>
+					</tr>
+					<tr>
+						<td width='15%'>Tanggal</td>
+						<td width='2%'>:</td>
+						<td><input type='tgl' id='tgl' size='12' class='input'></td>
+					</tr>
+					<tr>
+						<td width='15%'>Lama Pinjaman</td>
+						<td width='2%'>:</td>
+						<td><select name='lama' id='lama' class='input'>
+						<option value=''>-Pilih-</option>
+						<option value='6'>6 Bulan</option>
+						<option value='12'>12 Bulan</option>
+						<option value='24'>24 Bulan</option>
+						</select>
+						</td>
+					</tr>
+					<tr>
+						<td width='15%'>Bunga</td>
+							<td width='2%'>:</td>
+							<td>";
+							$table	= 'bunga';
+							$text	= "SELECT *	FROM $table";
+							$sql 	= mysqli_query($konek, $text);
+							$row	= mysqli_fetch_array($sql);
+							echo "<input type='text' name='bunga' value='$row[jumlah]%' id='bunga' size='5' disabled ></td>
+					</tr>
+					<tr>
+						<td width='15%'>Jumlah</td>
+						<td width='2%'>:</td>
+						<td><input type='text' name='jml' id='jml' size='15' class='input'></td>
+					</tr>
+					<tr>
+						<td colspan='3' align='center'>
+						<button class='ui-state-default ui-corner-all' id='hitung'>
+						<span class='ui-icon ui-icon-disk'></span>Hitung
+						</button>
+						<button class='ui-state-default ui-corner-all' id='tutup'>
+						<span class='ui-icon ui-icon-circle-close'></span>Tutup
+						</button>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<div id='menu-tombol1'>
+			<div id='tombol-tambah'>
+				<button class='ui-state-default ui-corner-all' id='tambah'>
+				<span class='ui-icon ui-icon-circle-plus'></span>Tambah
+				</button>
+			</div>
+			<div id='tombol-cari'>
+				Tanggal <input type='text' id='tgl1' size='10'> s/d <input type='text' id='tgl2' size='10'>
+				<button class='ui-state-default ui-corner-all' id='cari2'>
+					<span class='ui-icon ui-icon-search'></span>Cari
+				</button>
+			</div>
+			</div>
+			<div id='tampil_data1'></div>
+			<div id='tampil_data3'>
+			</div>
+			<div id='form_isian2'>
+				<table width='100%'>
+					<tr>
+						<td colspan='3' align='right'>
+							<button class='ui-state-default ui-corner-all' id='ajukan'>
+								<span class='ui-icon ui-icon-disk'></span>Ajukan
+							</button>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<div id='tabs-2'>
+			<p>Ketentuan :</p>
+			<p>Setiap Anggota meminjam harus memenuhi persyaratan sebagai berikut</p>
+			<ol>
+				<li>Foto Copy KTP Suami/Istri</li>
+				<li>Tidak Memiliki Tunggakan Sebelumnya</li>
+			</ol>
+		</div>
+	</div>
 </div>";
 ?>
